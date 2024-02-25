8a32d2d87da9f94f32af305ae673860d6de00b4f
nicdev4 <valf2k3@gmail.com>
2024-02-25 22:04:27 +0300

    404 page for /currency/:id Sparkline for currencies

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -3,11 +3,12 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {Observable} from "rxjs";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {LoadingComponent} from "../loading/loading.component";
+import {NotfoundComponent} from "../notfound/notfound.component";
 
 @Component({
   selector: 'app-currency',
   standalone: true,
-    imports: [HttpClientModule, LoadingComponent],
+    imports: [HttpClientModule, LoadingComponent, NotfoundComponent],
   templateUrl: './currency.component.html',
   styleUrl: './currency.component.css'
 })
@@ -15,6 +16,7 @@ export class CurrencyComponent implements OnInit{
   name: any | undefined;
   coin: Coin|undefined;
   loading = true;
+  error = false;
   constructor(private activatedRoute:ActivatedRoute, private http:HttpClient) {
   }
   ngOnInit() {
@@ -25,9 +27,13 @@ export class CurrencyComponent implements OnInit{
         this.coin = new Coin();
         this.coin.name = data.name;
         this.coin.image_url = data.image.large;
+        this.coin.sparkline_url= "https://www.coingecko.com/coins/"+data.image.large.toString().split('/')[5]+"/sparkline.svg";
         this.coin.price = data.market_data.current_price.usd;
         this.coin.description = data.description.en;
         this.coin.ath_percent_change = data.market_data.ath_change_percentage.usd.toString();
+        this.loading = false;
+      }, error: (data: any) => {
+        this.error = true;
         this.loading = false;
       }});
   }
@@ -37,6 +43,7 @@ class Coin{
   name: string|undefined;
   price: string|undefined;
   image_url: string|undefined;
+  sparkline_url: string|undefined;
   description: string|undefined;
   ath_percent_change: string|undefined;
 }
